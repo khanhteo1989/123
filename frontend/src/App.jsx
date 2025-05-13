d5fd160084d20b64487e9e399429eadb9d5617ba
KhangLee <150659223+khanhteo1989@users.noreply.github.com>
2025-05-13 21:20:33 +0700

    Delete frontend/src directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Chatbox from "./components/Chatbox";
-
-export default function App() {
-  return (
-    <div className="bg-gray-900 min-h-screen text-white">
-      <Chatbox />
-    </div>
-  );
-}
